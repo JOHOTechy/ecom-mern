8f95b71c9689033155657679e0f28fd24f24f17c
JOHOTechy <johotechy@gmail.com>
2026-05-18 22:10:09 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://98.93.167.80:5000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
